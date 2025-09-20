60f778506613f999da55e3605931111a3b1158db
Mr. HUNTER <im.a.hunter.one@gmail.com>
2025-09-20 23:03:51 +0000

    ضع الزر لجانب عدد الاطباء الشركاء

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { PlusCircle, Users } from 'lucide-react';
+import { PlusCircle, Users, SlidersHorizontal } from 'lucide-react';
 import { useLanguage } from '@/hooks/use-language';
 import { useDoctors } from '@/hooks/use-doctors';
 import { useAuth } from '@/hooks/use-auth';
@@ -10,7 +10,6 @@ import { Input } from '@/components/ui/input';
 import { Logo } from '@/components/logo';
 import { UserMenu } from './user-menu';
 import { DoctorFormDialog } from '@/components/doctor/doctor-form-dialog';
-import { SlidersHorizontal } from 'lucide-react';
 import { PartnerDashboard } from '../doctor/partner-dashboard';
 
 export function Header() {
