f48b5e409c883aef3180d7e27492f328123deac6
Mr. HUNTER <im.a.hunter.one@gmail.com>
2025-09-23 11:47:35 +0000

    اريد طريقة اضاقة المشروع يدويا على الفاير بيس

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -1,5 +1,10 @@
-// This file will be populated with your Firebase project's configuration
-// during the deployment process. You do not need to modify it manually.
+// This file will be populated with your Firebase project's configuration.
+// To manually connect your project:
+// 1. Go to the Firebase Console (https://console.firebase.google.com/).
+// 2. Create a new project or select an existing one.
+// 3. In your project's settings, find your web app's configuration object.
+// 4. Copy the config object and paste it here, replacing the empty object below.
+
 const firebaseConfig = {};
 
 export { firebaseConfig };
