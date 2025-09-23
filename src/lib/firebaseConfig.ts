b36519cf677536c842f0acb0b7e49ec179ca1718
Mr. HUNTER <im.a.hunter.one@gmail.com>
2025-09-23 11:46:38 +0000

    انا استخدم الهاتف

---
+++ src/lib/firebaseConfig.ts
@@ -0,0 +1,5 @@
+// This file will be populated with your Firebase project's configuration
+// during the deployment process. You do not need to modify it manually.
+const firebaseConfig = {};
+
+export { firebaseConfig };
