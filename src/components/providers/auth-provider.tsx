34476c1f623a181f0b93a2cd77766253936c1e6e
Mr. HUNTER <im.a.hunter.one@gmail.com>
2025-09-24 09:11:06 +0000

    كلمة مرور حساب Ahmed اريدها ان تتكون من 10 فقط

--- src/components/providers/auth-provider.tsx
+++ src/components/providers/auth-provider.tsx
@@ -36,8 +36,8 @@ export const AuthContext = createContext<AuthContextType | undefined>(undefined)
 const generatePassword = (length = 10) => {
     const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
     let password = "";
-    for (let i = 0, n = charset.length; i < n; ++i) {
-        password += charset.charAt(Math.floor(Math.random() * n));
+    for (let i = 0; i < length; i++) {
+        password += charset.charAt(Math.floor(Math.random() * charset.length));
     }
     return password;
 };
